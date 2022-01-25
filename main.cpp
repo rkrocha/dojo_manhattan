68958ea8a4cd3f584aa27bbde5ee6350ac8c60f4
rkrocha <rafaelkr7@gmail.com>
2022-01-24 21:03:20 -0300

    resolvido o manhattan_dist

--- main.cpp
+++ main.cpp
@@ -1,23 +1,41 @@
 #include <cmath>
 #include <iostream>
-using namespace std;
 
 class Point
 {
+	private:
+		const int x;
+		const int y;
+
+	public:
+		Point(int ax, int ay): x(ax), y(ay) {}
+		~Point() {}
+
+		float operator- (Point& rhs)
+		{
+			int x = this->x - rhs.x;
+			int y = this->y - rhs.y;
+			x *= x < 0 ? -1 : 1;
+			y *= y < 0 ? -1 : 1;
+			return static_cast<float>(x + y);
+		}
 
 };
 
 float	manhattan_dist(Point A, Point B)
 {
-
+	return A - B;
 }
 
 float	euclidean_dist(Point A, Point B)
 {
-
+	return 1;
 }
 
 int main ()
 {
-
+	if (manhattan_dist(Point(1, 1), Point(0, -3)) == 5)
+		std::cout << "Sucesso!" << std::endl;
+	else
+		std::cout << ":(" << std::endl;
 }
